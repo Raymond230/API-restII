ab5bb47fd41314b584a530fcf447c70d699f9f5a
Igor Oliveira <igoroliveira273@gmail.com>
2024-04-20 17:15:49 -0300

    Metodo get

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express');
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.send('hello,world igor')
+})
+
 const PORT = 3333;
 app.listen(PORT,()=> console.log(`Serve is running on Port ${PORT}` ));
